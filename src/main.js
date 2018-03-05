d0c9e7b77284b512db074df32f5207d0eb23d2ad
Dutch <dutch@unviral.com>
2018-03-05 16:28:13 -0500

    Add Google Analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App'
 
 import VueCookie from 'vue-cookie'
+import VueAnalytics from 'vue-analytics'
 
 Vue.use(VueCookie)
+
+Vue.use(VueAnalytics, {
+ id: 'UA-115004766-2'
+})
 
 Vue.config.productionTip = false
 
